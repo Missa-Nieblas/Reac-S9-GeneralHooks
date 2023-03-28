2414f069b55fe1d43849f5bfe892b9b6fd864b70
Missa <missa_nieblas@hotmail.com>
2023-03-27 23:14:39 -0700

    Se finaliza la S10, se realizo un nuevo hook para entender mejor el TodoApp y se agregaron 2 funciones simples

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,55 +1,53 @@
-import { useEffect, useReducer } from "react"
+//import { useEffect, useReducer } from "react"
+import { useTodo } from "../hooks/useTodo";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer"
+//import { todoReducer } from "./todoReducer"
 
-const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del sal',
-        done: false,
-    },
-    
-];
+//const initialState = [];
 
-const init = () => {
-    return JSON.parse( localStorage.getItem('todos')) || [];
-}
+//const init = () => {
+    //return JSON.parse( localStorage.getItem('todos')) || [];
+//}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer ,initialState, init);
+    //UseTodo
+    //todos, handleNewTodo,  handleDeleteTodo, handleToggleTodo
+    const { todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, handleToggleTodo} = useTodo();
 
-    useEffect(() => {
-      localStorage.setItem('todos', JSON.stringify( todos ) );
-    }, [todos])
+    //const [todos, dispatch] = useReducer(todoReducer ,initialState, init);
+
+    //useEffect(() => {
+      //localStorage.setItem('todos', JSON.stringify( todos ) );
+    //}, [todos])
     
 
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
+    //const handleNewTodo = (todo) => {
+        //const action = {
+            //type: '[TODO] Add Todo',
+            //payload: todo
+        //}
 
-        dispatch( action );
+        //dispatch( action );
         //console.log({todo});
 
-    }
-    const handleDeleteTodo = (id) => {
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        })
-    }
-    const handleToggleTodo = (id) => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        })
-    }
+    //}
+    //const handleDeleteTodo = (id) => {
+        //dispatch({
+            //type: '[TODO] Remove Todo',
+            //payload: id
+        //})
+    //}
+    //const handleToggleTodo = (id) => {
+        //dispatch({
+            //type: '[TODO] Toggle Todo',
+            //payload: id
+        //})
+    //}
   return (
     <>
-        <h1>TodoApp 10 <small>pendientes 2</small></h1>
+        <h1>TodoApp: {todosCount} <small>pendientes {pendingTodosCount}</small></h1>
         <hr/>
         <div className="row">
             <div className="col-7">
---
+++ src/hooks/useTodo.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer, useState } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = (initialState = []) => {
+
+    const [todos, dispatch] = useReducer(todoReducer ,initialState, init);
+    
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ) );
+      }, [todos])
+      
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+        //console.log({todo});
+
+    }
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+    const todosCount  = todos.length;
+    
+    const pendingTodosCount = todos.filter(todo=> !todo.done).length
+  
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount,
+        pendingTodosCount,
+    }
+}
