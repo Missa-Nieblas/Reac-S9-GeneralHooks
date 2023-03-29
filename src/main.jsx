cbdfa0785019c30a43619915e2c77091b5b8fb77
Missa <missa_nieblas@hotmail.com>
2023-03-28 23:28:36 -0700

    Inicio de S11 creacion de clases de distintas pagianas para el manejor del UseContex, utilizando tambien Nevbar

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,15 @@ import { Padre } from './07-tarea-memo/Padre'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { HooksApp } from './HooksApp'
 //import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
-
+//import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
+import {BrowserRouter, createBrowserRouter} from "react-router-dom";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   //<React.StrictMode>
-    <TodoApp />
-   // </React.StrictMode>,
+   <BrowserRouter>
+   {/* <React.StrictMode> */}
+    <MainApp/>
+   {/*  </React.StrictMode>,*/}
+   </BrowserRouter>
 )
